953c65ac13e698a4804d9eb8e6fd4eb76a00caf5
Dawid Buchwald <dawid.buchwald@hotmail.com>
2016-12-12 16:09:00 +0100

    Added support for Eclipse, disabled NetBeans download

--- setup/tools/delete_tools.sh
+++ setup/tools/delete_tools.sh
@@ -11,6 +11,16 @@ then
   rmdir ./netbeans
 fi
 
+if [ -f ./eclipse/eclipse.tar.gz ]
+then
+  rm -f ./eclipse/eclipse.tar.gz 
+fi
+
+if [ -d ./eclipse ]
+then
+  rmdir ./eclipse
+fi
+
 if [ -f ./wildfly/${WILDFLY_PACKAGE} ]
 then
   rm -f ./wildfly/${WILDFLY_PACKAGE}
--- setup/tools/download_tools.sh
+++ setup/tools/download_tools.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 . ./packages_setup.sh
 
-if ! [ -d ./netbeans ]
+#if ! [ -d ./netbeans ]
+#then
+#  echo "Creating directory netbeans..."
+#  mkdir -p ./netbeans
+#  echo "...done"
+#fi
+#
+#if ! [ -f ./netbeans/${NETBEANS_INSTALLER} ]
+#then
+#  echo "Downloading NetBeans installer..."
+#  wget -O ./netbeans/${NETBEANS_INSTALLER} --quiet --tries=0 --continue http://download.netbeans.org/netbeans/${NETBEANS_VERSION}/final/bundles/${NETBEANS_INSTALLER} 
+#  echo "...done"
+#fi
+#
+#if ! [ -x ./netbeans/${NETBEANS_INSTALLER} ]
+#then
+#  echo "Setting NetBeans installer to be executable..."
+#  chmod u+x ./netbeans/${NETBEANS_INSTALLER}
+#  echo "...done"
+#fi
+
+if ! [ -d ./eclipse ]
 then
-  echo "Creating directory netbeans..."
-  mkdir -p ./netbeans
+  echo "Creating directory eclipse..."
+  mkdir -p ./eclipse
   echo "...done"
 fi
 
-if ! [ -f ./netbeans/${NETBEANS_INSTALLER} ]
+if ! [ -f ./eclipse/eclipse.tar.gz ]
 then
-  echo "Downloading NetBeans installer..."
-  wget -O ./netbeans/${NETBEANS_INSTALLER} --quiet --tries=0 --continue http://download.netbeans.org/netbeans/${NETBEANS_VERSION}/final/bundles/${NETBEANS_INSTALLER} 
-  echo "...done"
-fi
-
-if ! [ -x ./netbeans/${NETBEANS_INSTALLER} ]
-then
-  echo "Setting NetBeans installer to be executable..."
-  chmod u+x ./netbeans/${NETBEANS_INSTALLER}
+  echo "Downloading eclipse package..."
+  wget -O .eclipse/eclipse.tar.gz --quied --tries=0 --continue http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/1a/eclipse-jee-neon-1a-linux-gtk-x86_64.tar.gz
   echo "...done"
 fi
 
