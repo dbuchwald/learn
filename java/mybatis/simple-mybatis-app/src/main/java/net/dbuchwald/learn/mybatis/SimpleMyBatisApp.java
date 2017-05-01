b3c649aa1b6c879bd1fb601cc8a73faeaac5937a
Dawid Buchwald <dawid.buchwald@hotmail.com>
2017-05-01 18:53:36 +0200

    Completed sample MyBatis application

--- java/mybatis/simple-mybatis-app/src/main/java/net/dbuchwald/learn/mybatis/SimpleMyBatisApp.java
+++ java/mybatis/simple-mybatis-app/src/main/java/net/dbuchwald/learn/mybatis/SimpleMyBatisApp.java
@@ -1,7 +1,11 @@
 package net.dbuchwald.learn.mybatis;
 
 import net.dbuchwald.learn.mybatis.model.Customer;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -11,5 +15,25 @@ import java.util.List;
 public class SimpleMyBatisApp 
 {
     public static void main(String[] args) {
+        String propertiesFileName = "mybatis/config/mybatis-config.xml";
+        InputStream inputStream = SimpleMyBatisApp.class.getClassLoader().getResourceAsStream(propertiesFileName);
+
+        if (inputStream == null) {
+            System.err.println("Unable to load property file: " + propertiesFileName);
+        } else {
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            SqlSession session = sqlSessionFactory.openSession();
+            try {
+                //Customer customer = session.selectOne("mybatis.mappers.CustomerMapper.selectCustomer", 1);
+                List<Customer> customers = session.selectList("mybatis.mappers.CustomerMapper.selectAllCustomers");
+                for (Customer customer: customers) {
+                    System.out.println(customer.toString());
+                }
+                System.out.println("Size: " + customers.size());
+            } finally {
+                session.close();
+            }
+        }
     }
 }
