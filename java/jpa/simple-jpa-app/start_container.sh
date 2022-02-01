a896a76854263125c0eb8e6789ecc5018f24f262
Dawid Buchwald <dawid.buchwald@hotmail.com>
2022-02-01 23:56:05 +0100

    Sorted out issue with container start time

--- java/jpa/simple-jpa-app/start_container.sh
+++ java/jpa/simple-jpa-app/start_container.sh
@@ -2,7 +2,9 @@
 
 docker run --detach --rm --name db_server -p 3306:3306 --env MARIADB_ROOT_PASSWORD=MariaDBRootPassword  mariadb:latest
 
-sleep 10
+printf "Waiting for container to start..."
+while ! mariadb --user=root --password=MariaDBRootPassword --protocol=tcp --host=localhost --no-beep --execute=exit 2>/dev/null; do :; printf "."; sleep 1; done
+printf "done\n"
 
 mariadb --user=root --password=MariaDBRootPassword --protocol=tcp --host=localhost <<EOT
     DROP DATABASE IF EXISTS mydb;
