4d3b4bca94e19adada11a5dd8f564db1546b8a72
Dawid Buchwald <dawid.buchwald@hotmail.com>
2022-05-12 12:20:35 +0200

    Moved configuration data to ConfigMap/Secret objects, added StorageClass and updated documentation

--- k8s/k3s/apiserver/deploy.sh
+++ k8s/k3s/apiserver/deploy.sh
@@ -2,10 +2,14 @@
 
 kubectl apply -f apidb-pv.yaml
 kubectl apply -f api-namespace.yaml
+kubectl apply -f apidb-storageclass.yaml
 kubectl apply -f apidb-pvc.yaml
+kubectl apply -f apidb-configmap.yaml
+kubectl apply -f apidb-secret.yaml
 kubectl apply -f apidb-deployment.yaml
 kubectl apply -f apidb-service.yaml
 
+kubectl apply -f api-configmap.yaml
 kubectl apply -f api-deployment.yaml
 kubectl apply -f api-service.yaml
 kubectl apply -f api-ingress.yaml
