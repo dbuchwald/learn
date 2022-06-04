7fa673a39a9bb5a21755b18ee3038cb99f33437c
Dawid Buchwald <dawid.buchwald@hotmail.com>
2022-06-04 16:03:19 +0200

    Fourth step of module tutorial

--- go/tutorial/hello_mod/hello_mod.go
+++ go/tutorial/hello_mod/hello_mod.go
@@ -15,7 +15,7 @@ func main() {
     log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
     // Request a greeting message.
-    message, err := greetings.Hello("")
+    message, err := greetings.Hello("Gladys")
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
